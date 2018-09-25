842a3b6aff6a8438fd31bac4e229503ec9366d79
Yassin AJDI <yassin.ajdi@gmail.com>
2018-09-25 19:47:36 +0100

    Add androidx stable deps

--- app/src/main/java/com/ajdi/yassin/popularmoviespart1/MainActivity.java
+++ app/src/main/java/com/ajdi/yassin/popularmoviespart1/MainActivity.java
@@ -1,6 +1,6 @@
 package com.ajdi.yassin.popularmoviespart1;
 
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
