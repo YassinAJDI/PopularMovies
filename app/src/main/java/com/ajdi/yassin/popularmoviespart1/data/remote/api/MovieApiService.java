7ce9005a41aca68a91bb369b29cb4e2d1d69764d
Yassin AJDI <yassin.ajdi@gmail.com>
2018-09-27 21:23:43 +0100

    Add paging requred classes

--- app/src/main/java/com/ajdi/yassin/popularmoviespart1/data/remote/api/MovieApiService.java
+++ app/src/main/java/com/ajdi/yassin/popularmoviespart1/data/remote/api/MovieApiService.java
@@ -2,16 +2,17 @@ package com.ajdi.yassin.popularmoviespart1.data.remote.api;
 
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.Query;
 
 /**
  * TheMovieDB API communication setup via Retrofit.
- *
+ * <p>
  * Created by Yassin Ajdi.
  */
 public interface MovieApiService {
 
     @GET("movie/popular")
-    Call<MoviesResponse> getPopularMovies();
+    Call<MoviesResponse> getPopularMovies(@Query("page") int page);
 
     @GET("movie/top_rated")
     Call<MoviesResponse> getTopRatedMovies();
