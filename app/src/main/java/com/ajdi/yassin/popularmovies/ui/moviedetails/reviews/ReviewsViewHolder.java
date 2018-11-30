8fb3f80bfbfba8e2d8505f01d213c180e580b907
Yassin AJDI <yassin.ajdi@gmail.com>
2018-11-30 16:42:04 +0000

    Add review's round drawables

--- app/src/main/java/com/ajdi/yassin/popularmovies/ui/moviedetails/reviews/ReviewsViewHolder.java
+++ app/src/main/java/com/ajdi/yassin/popularmovies/ui/moviedetails/reviews/ReviewsViewHolder.java
@@ -7,6 +7,8 @@ import android.view.ViewGroup;
 
 import com.ajdi.yassin.popularmovies.data.local.model.Review;
 import com.ajdi.yassin.popularmovies.databinding.ItemReviewBinding;
+import com.amulyakhare.textdrawable.TextDrawable;
+import com.amulyakhare.textdrawable.util.ColorGenerator;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -30,7 +32,19 @@ public class ReviewsViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void bindTo(final Review review) {
-        binding.textAuthor.setText(review.getAuthor());
+        String userName = review.getAuthor();
+
+        // review user image
+        ColorGenerator generator = ColorGenerator.MATERIAL;
+        int color = generator.getRandomColor();
+        TextDrawable drawable = TextDrawable.builder()
+                .buildRound(userName.substring(0, 1).toUpperCase(), color);
+        binding.imageAuthor.setImageDrawable(drawable);
+
+        // review's author
+        binding.textAuthor.setText(userName);
+
+        // review's content
         binding.textContent.setText(review.getContent());
 
         binding.executePendingBindings();
