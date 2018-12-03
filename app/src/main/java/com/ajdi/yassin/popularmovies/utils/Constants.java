21edd9984a18fd95fd65807539a371fcfb1c4ec3
Yassin AJDI <yassin.ajdi@gmail.com>
2018-12-03 14:55:40 +0000

    Use HTTPS for urls

--- app/src/main/java/com/ajdi/yassin/popularmovies/utils/Constants.java
+++ app/src/main/java/com/ajdi/yassin/popularmovies/utils/Constants.java
@@ -4,8 +4,8 @@ package com.ajdi.yassin.popularmovies.utils;
  * @author Yassin Ajdi.
  */
 public class Constants {
-    public static final String IMAGE_BASE_URL = "http://image.tmdb.org/t/p/";
-    public static final String YOUTUBE_WEB_URL = "http://www.youtube.com/watch?v=";
+    public static final String IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
+    public static final String YOUTUBE_WEB_URL = "https://www.youtube.com/watch?v=";
     public static final String IMAGE_SIZE_W185 = "w185";
     public static final String PROFILE_SIZE_W185 = "w185";
     public static final String BACKDROP_SIZE = "w780";
