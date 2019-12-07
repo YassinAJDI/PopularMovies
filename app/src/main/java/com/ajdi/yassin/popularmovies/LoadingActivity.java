818f22adf93a6e1fc444e44a08d921d69f3068e5
saechimdaeki <anima94@naver.com>
2019-12-08 01:41:31 +0900

    done

--- app/src/main/java/com/ajdi/yassin/popularmovies/LoadingActivity.java
+++ app/src/main/java/com/ajdi/yassin/popularmovies/LoadingActivity.java
@@ -3,18 +3,21 @@ package com.ajdi.yassin.popularmovies;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.DisplayMetrics;
+import android.view.WindowManager;
 import android.widget.ImageView;
 
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.Target;
 
 public class LoadingActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading);
-        ImageView imageView=(ImageView)findViewById(R.id.image_loading);
-        Glide.with(getBaseContext()).load(R.raw.marvle).into(imageView);
+        ImageView imageView=findViewById(R.id.image_loading);
+        Glide.with(getBaseContext()).load(R.raw.marvel).fitCenter().override(Target.SIZE_ORIGINAL,Target.SIZE_ORIGINAL).into(imageView);
         startLoading();
     }
     private void startLoading() {
@@ -24,6 +27,6 @@ public class LoadingActivity extends Activity {
             public void run() {
                 finish();
             }
-        }, 8000);
+        }, 4600);
     }
 }
